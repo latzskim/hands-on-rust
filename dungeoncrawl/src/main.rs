a469f2bdae313df0533a6e615715e1432605b3eb
karolmiksztal <karolmiksztal@gmail.com>
2022-07-25 18:26:55 +0200

    Added ECS

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,10 +1,16 @@
 mod camera;
+mod components;
 mod map;
 mod map_builder;
-mod player;
+mod spawner;
+mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
+
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
 
@@ -12,28 +18,42 @@ mod prelude {
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
 
     pub use crate::camera::*;
+    pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
-    pub use crate::player::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
 }
 
-use map::Map;
 use prelude::*;
 
 struct State {
-    map: Map,
-    player: Player,
-    camera: Camera,
+    ecs: World,
+    resouces: Resources,
+    systems: Schedule,
 }
 
 impl State {
     fn new() -> Self {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+
+        spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
+
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start),
+            ecs: ecs,
+            resouces: resources,
+            systems: build_scheduler(),
         }
     }
 }
@@ -46,9 +66,9 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
 
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+        self.resouces.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resouces);
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
