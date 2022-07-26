2b81faae538da5432e1248209b06a3dfd87f59b0
karolmiksztal <karolmiksztal@gmail.com>
2022-07-26 20:16:34 +0200

    Added combat system.

--- dungeoncrawl/src/systems/random_move.rs
+++ dungeoncrawl/src/systems/random_move.rs
@@ -3,10 +3,13 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+#[read_component(Player)]
+#[read_component(Health)]
+pub fn random_move(ecs: &SubWorld, commands: &mut CommandBuffer) {
     let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
+    let mut positions = <(Entity, &Point, &Health)>::query();
 
-    movers.iter_mut(ecs).for_each(|(entity, pos, _)| {
+    movers.iter(ecs).for_each(|(entity, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
         let mut destination = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
@@ -16,12 +19,36 @@ pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         };
 
         destination += *pos;
-        commands.push((
-            (),
-            WantsToMove {
-                entity: *entity,
-                destination,
-            },
-        ));
+        let mut attacked = false;
+        positions
+            .iter(ecs)
+            .filter(|(_, target_pos, _)| **target_pos == destination)
+            .for_each(|(victim, _, _)| {
+                if ecs
+                    .entry_ref(*victim)
+                    .unwrap()
+                    .get_component::<Player>()
+                    .is_ok()
+                {
+                    commands.push((
+                        (),
+                        WantsToAttack {
+                            attacker: *entity,
+                            victim: *victim,
+                        },
+                    ));
+                }
+                attacked = true;
+            });
+
+        if !attacked {
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        }
     });
 }
