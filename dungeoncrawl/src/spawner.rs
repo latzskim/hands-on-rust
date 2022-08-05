33c2aa480bc71b8432319259e7e73c971bfc6b46
karolmiksztal <karolmiksztal@gmail.com>
2022-08-05 19:40:56 +0200

    Add Field of View.

--- dungeoncrawl/src/spawner.rs
+++ dungeoncrawl/src/spawner.rs
@@ -9,9 +9,23 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             glyph: to_cp437('@'),
         },
         Health {
-            current: 1,
+            current: 10,
             max: 20,
         },
+        FieldOfView::new(8),
+    ));
+}
+
+pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Item,
+        AmuletOfYala,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('/'),
+        },
+        Name("Amulet of Yala".to_string()),
     ));
 }
 
@@ -34,6 +48,7 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
             max: hp,
         },
         Name(name),
+        FieldOfView::new(6),
     ));
 }
 
